d85d78cfc838ecbc79dd2a40c12bd0c6416bbcb6
Mateus Pandino <matpandino@gmail.com>
2023-06-03 20:50:25 -0300

    add tailwind ui dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 } satisfies Config;
